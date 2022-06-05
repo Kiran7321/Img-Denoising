0eacdca0fca7ec16b694904393f0a3d9dd5b399a
Kiran7321 <89258260+Kiran7321@users.noreply.github.com>
2022-06-05 08:21:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 # Importing the required packages
 import math
 import streamlit as st
+
+!pip install opencv-python
+!apt update && apt install -y libsm6 libxext6
+!apt-get install -y libxrender-dev
+
 import cv2      
 import os
 import urllib
